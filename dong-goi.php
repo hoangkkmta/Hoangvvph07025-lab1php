670d9d8441362b38da3266d5cebba70f4d5cb5b4
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-04-21 22:18:53 +0700

    Create dong-goi.php

---
+++ dong-goi.php
@@ -0,0 +1,20 @@
+<?php
+ class Animal{
+   private $name="Hoangvv";
+   public function setname($newname){
+       $this->name=$newname;
+   }
+   public function getname(){
+       return $this->name;
+   }
+
+ }
+ class obj extends Animal{
+     public function getage(){
+         return this->age;
+     }
+ }
+ $nam=new obj();
+ $nam->setage(29);
+ echo $nam->getage();
+?>
