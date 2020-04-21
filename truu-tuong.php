0faf2ff4546295d6c24e16545c47c3a8fb671a66
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-04-21 17:11:03 +0700

    Create truu-tuong.php

---
+++ truu-tuong.php
@@ -0,0 +1,35 @@
+<?php
+//vd1
+ class SinhVien{
+
+    var $hoten;
+    var $ngaysinh;
+    var $gioitinh;
+    var $tuoi;
+    var $sdtbo;
+    var $mssv;
+    var $cmnd;
+    var $avatar;
+    var $diachi;
+    var $sdt;  
+    function dihoc(){}
+    function hoclai(){}
+    function dithi(){}
+    function lamquiz(){}
+    function lamlab(){}
+    function diemdanh(){}
+//vd2
+class PhepTinh{
+    var $soam;
+    var $soduong;
+    var $songuyento;
+    var $sochan;
+    var $sole;
+    function tong()
+    function hieu()
+    function tich()
+    function thuong()
+
+}
+
+?>
